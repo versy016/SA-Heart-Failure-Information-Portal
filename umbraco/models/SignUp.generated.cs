faf2e212728094b34367632289c366aee2855766
Vozpa001 <Vozpa001@mymail.unisa.edu.au>
2022-09-04 17:57:13 +0930

    Fixed login and signup page linking

--- umbraco/models/SignUp.generated.cs
+++ umbraco/models/SignUp.generated.cs
@@ -18,7 +18,7 @@ using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Web.Common.PublishedModels
 {
-	/// <summary>Sign Up</summary>
+	/// <summary>SignUpPage</summary>
 	[PublishedModel("signUp")]
 	public partial class SignUp : PublishedContentModel
 	{
