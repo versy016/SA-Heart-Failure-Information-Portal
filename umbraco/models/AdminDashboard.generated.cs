69ad1b39b191d916e0162ce34d6fa0cffb0350b4
Vozpa001 <Vozpa001@mymail.unisa.edu.au>
2022-12-16 09:20:06 +1030

    Implemented the JS functionality for the various log-in and sign-up user options. Created the template for the Admin dashboard, created the template for the clinican and committee registration page

--- umbraco/models/AdminDashboard.generated.cs
+++ umbraco/models/AdminDashboard.generated.cs
@@ -18,14 +18,14 @@ using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Web.Common.PublishedModels
 {
-	/// <summary>CliniciansPortal</summary>
-	[PublishedModel("cliniciansPage")]
-	public partial class CliniciansPage : PublishedContentModel
+	/// <summary>Admin Dashboard</summary>
+	[PublishedModel("adminDashboard")]
+	public partial class AdminDashboard : PublishedContentModel
 	{
 		// helpers
 #pragma warning disable 0109 // new is redundant
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "10.3.1+86fb24a")]
-		public new const string ModelTypeAlias = "cliniciansPage";
+		public new const string ModelTypeAlias = "adminDashboard";
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "10.3.1+86fb24a")]
 		public new const PublishedItemType ModelItemType = PublishedItemType.Content;
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "10.3.1+86fb24a")]
@@ -34,14 +34,14 @@ namespace Umbraco.Cms.Web.Common.PublishedModels
 			=> PublishedModelUtility.GetModelContentType(publishedSnapshotAccessor, ModelItemType, ModelTypeAlias);
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "10.3.1+86fb24a")]
 		[return: global::System.Diagnostics.CodeAnalysis.MaybeNull]
-		public static IPublishedPropertyType GetModelPropertyType<TValue>(IPublishedSnapshotAccessor publishedSnapshotAccessor, Expression<Func<CliniciansPage, TValue>> selector)
+		public static IPublishedPropertyType GetModelPropertyType<TValue>(IPublishedSnapshotAccessor publishedSnapshotAccessor, Expression<Func<AdminDashboard, TValue>> selector)
 			=> PublishedModelUtility.GetModelPropertyType(GetModelContentType(publishedSnapshotAccessor), selector);
 #pragma warning restore 0109
 
 		private IPublishedValueFallback _publishedValueFallback;
 
 		// ctor
-		public CliniciansPage(IPublishedContent content, IPublishedValueFallback publishedValueFallback)
+		public AdminDashboard(IPublishedContent content, IPublishedValueFallback publishedValueFallback)
 			: base(content, publishedValueFallback)
 		{
 			_publishedValueFallback = publishedValueFallback;
